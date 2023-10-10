171b4c05e8dd67ebd820b9a9c8edd2623fca03ab
Teresa Cristina <136469627+teresacristinalba@users.noreply.github.com>
2023-10-10 10:09:22 -0300

    Create questao1.java

---
+++ questao1.java
@@ -0,0 +1,9 @@
+public class Questao1{
+    public static void main(){
+        int contador = 1;
+        do{
+            System.out.println("Eu gosto de estudar Algaritmos!");
+            contador = + 1;
+        }while(contador <= 20);
+        }
+    }
